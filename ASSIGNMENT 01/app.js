f1b6088075f0637b2be436ae2fbaf6c7f7413c85
Arsalan Fayyaz <arsalanfayyaz37@gmail.com>
2023-09-11 14:18:22 +0500

    JS ASSIGNMENTS

---
+++ ASSIGNMENT 01/app.js
@@ -0,0 +1,164 @@
+//--------------------------- Chapter 1 (Alerts) ---------------------------------
+
+// Q1. Alert these following (individually):
+// I. First Name
+// II. Last Name
+// III. Email
+// IV. Phone Number
+// V. Password
+
+alert("Arsalan");
+alert("Fayyaz");
+alert("arsalanfayyaz37@gmail.com");
+alert("03233339949");
+alert("password");
+
+//Q2. Correct this statement: alert"You're learning JavaScript!";
+
+alert("You're learning JavaScript!");
+
+//Q3. Code an alert statement displaying any message you like.
+
+alert("I Am Learning Javascript");
+
+// ------------------------ Chapter 2 (Variables for string) -----------------------------
+
+//Q1. Declare any variable in the camelCase format.
+
+var firstName;
+
+// Q2. Declare a variable of your choice without defining it. Then, in a 
+// second statement, assign it a string of your choice.
+
+var firstName;
+firstName="Arsalan";
+
+// Q3. Declare the variable teamName and Alert your Team name.
+
+var teamName;
+teamName="TEAM ML"
+alert(teamName);
+
+// Q4. This statement has already been coded. var bestMan = "Charlie";
+// Assign the variable a new string.
+
+var bestMan = "Charlie";
+bestMan = "John";
+
+// ------------------------ Chapter 3 (Variables for numbers) -----------------------------
+
+// Q1. Declare a variable “caseQty”
+
+var caseQty;
+
+// Q2. Assign to the variable caseQty, which has already been declared, 
+// the value 144.
+
+caseQty = 144;
+
+// Q3. Rewrite this statement so the variable can be used in a math 
+// operation. var num = "9";
+
+var num = "9"; 
+num = 9;
+
+// Q4. In one statement declare a variable. In a second statement assign 
+// it the sum of 2 numbers.
+
+var sum;
+sum = 1+2;
+
+// Q5. What is the value of orderTotal?
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// Try it yourself.
+
+//The value of orderTotal is 110
+
+// Q6. In the first statement declare a variable and assign it a number. In 
+// the second statement, change the value of the variable by adding 
+// it together with a number.
+
+var num1 = 10;
+num1 = num1 + 10;
+
+// ----------------- Chapter 4 (Variable names Legal and Illegal) -------------------
+
+// Q1. Correct this statement.
+// vaQr product cost = 3.45;
+
+vaQrproductcost = 3.45;
+
+// Q2. Rewrite this using camelCase.
+// var Nameofband;
+
+var nameOfBand; 
+
+// Q3. In a single statement declare a legally-named variable and assign a 
+// number to it.
+
+legalVariable = 10;
+
+// Q4. Declare a variable that is a combination of your first and last 
+// names. Use camelCase.
+
+var arsalanFayyaz;
+
+// Q5. List the legal and Illegal Variables.
+
+//legal variables
+
+var num1;
+var $num1;
+var num_1;
+
+//illegal variables
+
+// var num 1;
+// var @num1;
+// var num-1;
+
+// ------------------------ Chapter 5 (Math Expression I) -----------------------------
+
+// Q1. What is the name and symbol of the arithmetic operator that 
+// gives you the remainder when one number is divided by another?
+
+//Name is modulus and symbol is (  %  ).
+
+// Q2. What is the value of num?
+//var num = 20 % 6;
+
+//value of num is 2.
+
+// Q3. In a single statement, declare the variable largeNum and assign it 
+// the result of 1,000 multiplied by 2,000.
+
+var largeNum = 1000*2000;
+
+// Q4. Assign to a variable the value represented by one variable 
+// subtracted from the value represented by another variable
+
+var one = 10;
+var two = 5;
+var result = one - two;
+
+// Q5. Assign to a variable the remainder when one number is divided by 
+// another. The variable hasn't been declared beforehand. Make up 
+// the variable name.
+
+var numberOne = 10;
+var numbertwo = 5;
+var remainder = one % two;
+
+// Q6. Code an alert that displays the result of a multiplication on 2 
+// numbers.
+
+alert(10*5);
+
+// ====================================== THE END ======================================
+
+
+
+
+
