462e8f16e0b237b8b096624511fea2c50332e969
Arsalan Fayyaz <114776250+ArsalanFayyaz37@users.noreply.github.com>
2023-10-19 16:44:20 +0500

    Add files via upload

---
+++ ASSIGNMENT 09/app.js
@@ -0,0 +1,45 @@
+var hourHeading = document.getElementById("hour");
+var mintHeading = document.getElementById("mint");
+var secHeading = document.getElementById("sec");
+
+var hour = 0;
+var mint = 0;
+var sec = 0;
+
+var interval;
+
+function timer() {
+    sec++;
+    secHeading.innerHTML = sec;
+    if (sec >= 60) {
+        mint++;
+        mintHeading.innerHTML = mint;
+        sec = 0;
+    } else if (mint >= 60) {
+        hour++;
+        hourHeading.innerHTML = hour;
+        mint = 0;
+    }
+}
+function start(){
+    var start = document.getElementById("startbtn");
+    interval = setInterval(timer,1000);
+    start.disabled = true;
+}
+function pause(){
+    var start = document.getElementById("startbtn");
+    clearInterval(interval);
+    start.disabled = false;
+}
+function reset(){
+
+    hour = 0;
+    mint = 0;
+    sec = 0;
+
+    hourHeading.innerHTML = "00";
+    mintHeading.innerHTML = "00";
+    secHeading.innerHTML = "00";
+    pause();
+    setInterval(interval);
+}
